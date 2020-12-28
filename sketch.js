49b6cff60a12befc1c47c64f3982dc8eb8dca72d
Raghavkhandelwal7 <71634967+Raghavkhandelwal7@users.noreply.github.com>
2020-12-28 11:38:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+hr=hour();
+mn=minute();
+sc=second();
+angleMode(DEGREES);
+;function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
